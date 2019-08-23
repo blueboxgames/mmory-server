6dfb8352c49f45c7c5408e794270f9f2c7e40fac
Fudo Sakurai <fudo.sakurai@gmail.com>
2019-08-23 16:18:05 +0430

    fcm push functionality added

--- extension/src/com/gerantech/mmory/sfs/inbox/InboxBroadcastMessageHandler.java
+++ extension/src/com/gerantech/mmory/sfs/inbox/InboxBroadcastMessageHandler.java
@@ -2,6 +2,7 @@ package com.gerantech.mmory.sfs.inbox;
 
 import com.gerantech.mmory.libs.Commands;
 import com.gerantech.mmory.libs.utils.InboxUtils;
+import com.gerantech.mmory.libs.utils.FCMUtils;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
@@ -15,7 +16,7 @@ public class InboxBroadcastMessageHandler extends BaseClientRequestHandler
 {
     public void handleClientRequest(User sender, ISFSObject params)
     {
-        Collection<Integer> receivers = params.getIntArray("receiverIds");
+        Collection<Integer> receivers = params.getIntArray("receivers");
         Integer[] receiverIds = receivers.toArray(new Integer[receivers.size()]);
         String data = params.containsKey("data") ? params.getText("data") : null;
 
@@ -23,8 +24,8 @@ public class InboxBroadcastMessageHandler extends BaseClientRequestHandler
         for( int i = 0; i < receiverIds.length; i++ )
             delivered += InboxUtils.getInstance().send(params.getInt("type"), params.getUtfString("text"), params.getInt("senderId"), receiverIds[i], data);
 
-        //if( params.getBool("isPush") )
-        //    OneSignalUtils.getInstance().send(params.getUtfString("text"), params.getText("data"), receiverIds);
+        if( params.getBool("isPush") )
+            FCMUtils.getInstance().send(params.getUtfString("text"), data, receiverIds);
 
         params.putInt("delivered", delivered);
         send(Commands.INBOX_BROADCAST, params, sender);
