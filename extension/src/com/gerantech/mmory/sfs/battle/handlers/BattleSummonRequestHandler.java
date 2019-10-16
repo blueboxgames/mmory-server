099509a32fe798ad36cda00bf1ca5297661aa4fe
Mansour <mansurjavadi@gmail.com>
2019-10-16 13:42:52 +0330

    temp backward compatibility added

--- extension/src/com/gerantech/mmory/sfs/battle/handlers/BattleSummonRequestHandler.java
+++ extension/src/com/gerantech/mmory/sfs/battle/handlers/BattleSummonRequestHandler.java
@@ -1,8 +1,8 @@
 package com.gerantech.mmory.sfs.battle.handlers;
 
-import com.gerantech.mmory.sfs.battle.BattleRoom;
 import com.gerantech.mmory.core.battle.BattleField;
 import com.gerantech.mmory.libs.utils.BattleUtils;
+import com.gerantech.mmory.sfs.battle.BattleRoom;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
@@ -16,7 +16,8 @@ public class BattleSummonRequestHandler extends BaseClientRequestHandler
 			if( room.getState() < BattleField.STATE_1_CREATED || room.getState() > BattleField.STATE_3_PAUSED )
 				return;
 			int side = room.getPlayerGroup(sender);
-		room.summonUnit(side, params.getInt("t"), params.getDouble("x"), params.getDouble("y"), params.getDouble("time"));
+			double time = params.containsKey("time") ? params.getDouble("time") : System.currentTimeMillis() - 200;
+			room.summonUnit(side, params.getInt("t"), params.getDouble("x"), params.getDouble("y"), time);
 	} catch (Exception | Error e) { e.printStackTrace(); };
 }
 }
