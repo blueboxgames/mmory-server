aa916fb37354c38b3fe9b32272e01cb7956b7d5b
Mansour <mansurjavadi@gmail.com>
2020-02-05 11:13:52 +0330

    db getter added

--- utils/src/com/gerantech/mmory/libs/BBGClientRequestHandler.java
+++ utils/src/com/gerantech/mmory/libs/BBGClientRequestHandler.java
@@ -1,13 +1,17 @@
 package com.gerantech.mmory.libs;
 
+import java.util.List;
+
+import com.smartfoxserver.v2.db.IDBManager;
 import com.smartfoxserver.v2.entities.User;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
 
-import java.util.List;
-
 public class BBGClientRequestHandler extends BaseClientRequestHandler
 {
+    protected IDBManager dbManager() {
+        return getParentExtension().getParentZone().getDBManager();
+    }
 
     protected void send(String cmdName, int response, ISFSObject params, User recipient)
     {
