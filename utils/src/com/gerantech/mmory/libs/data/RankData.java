7289954e596954487e60775bbb0b56fb0d732143
Mansour <mansurjavadi@gmail.com>
2020-02-06 13:21:37 +0330

    status added to rank

--- utils/src/com/gerantech/mmory/libs/data/RankData.java
+++ utils/src/com/gerantech/mmory/libs/data/RankData.java
@@ -3,12 +3,18 @@ package com.gerantech.mmory.libs.data;
 import java.io.Serializable;
 public class RankData implements Serializable
 {
+	static public final int STATUS_NAN = -1;
+	static public final int STATUS_OFF = 0;
+	static public final int STATUS_ON = 1;
+	static public final int STATUS_BUSY = 2;
+	
 	private static final long serialVersionUID = 1L;
 	public String name;
 	public int point;
+	public int status = 0;
 	public RankData(String name, int point)
 	{
-	    super();
+		super();
 		this.name = name;
 		this.point = point;
 	}
