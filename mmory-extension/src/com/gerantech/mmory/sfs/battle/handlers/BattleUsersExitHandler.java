9306071876520e34d44a626c4612cff0c669edae
Mansour <mansurjavadi@gmail.com>
2020-03-11 23:45:32 +0330

    room leave and user exit handled

--- mmory-extension/src/com/gerantech/mmory/sfs/battle/handlers/BattleUsersExitHandler.java
+++ mmory-extension/src/com/gerantech/mmory/sfs/battle/handlers/BattleUsersExitHandler.java
@@ -6,6 +6,7 @@ import com.gerantech.mmory.libs.BBGRoom;
 import com.gerantech.mmory.libs.data.RankData;
 import com.gerantech.mmory.libs.utils.BattleUtils;
 import com.gerantech.mmory.libs.utils.RankingUtils;
+import com.gerantech.mmory.libs.utils.RoomsUtils;
 import com.smartfoxserver.v2.core.ISFSEvent;
 import com.smartfoxserver.v2.core.SFSEventParam;
 import com.smartfoxserver.v2.core.SFSEventType;
@@ -26,14 +27,20 @@ public class BattleUsersExitHandler extends BaseServerEventHandler
             return;
 
         BBGRoom room = BattleUtils.getInstance().findByUser(user, BattleField.STATE_0_WAITING, BattleField.STATE_5_DISPOSED);
-        if( room == null )
-            return;
-        Player p = BattleUtils.getInstance().getGame(user).player;
-        BattleUtils.getInstance().leave(room, user);
+        if( room != null )
+        {
+            Player p = BattleUtils.getInstance().getGame(user).player;
+            BattleUtils.getInstance().leave(room, user);
+    
+            // user status update
+            if (!room.isSpectator(user))
+                RankingUtils.getInstance().update(p.id, p.nickName, p.get_point(), arg.getType().equals(SFSEventType.USER_DISCONNECT) ? RankData.STATUS_OFF : RankData.STATUS_ON);
+        }
 
-        // user status update
-        if (!room.isSpectator(user))
-            RankingUtils.getInstance().update(p.id, p.nickName, p.get_point(), arg.getType().equals(SFSEventType.USER_DISCONNECT) ? RankData.STATUS_OFF : RankData.STATUS_ON);
+        room = RoomsUtils.getInstance().findByUser(user);
+        if( room != null){trace("sssssssss", room);
+        RoomsUtils.getInstance().leave(room, user);
+    }
 
     }
 }
