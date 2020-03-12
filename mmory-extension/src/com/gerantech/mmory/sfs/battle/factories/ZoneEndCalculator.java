4a5f2eb7d925a2dc7911839e2cc9b84a161f2a26
Mansour <mansurjavadi@gmail.com>
2020-03-12 22:58:35 +0330

    new zone calculator

---
+++ mmory-extension/src/com/gerantech/mmory/sfs/battle/factories/ZoneEndCalculator.java
@@ -0,0 +1,35 @@
+package com.gerantech.mmory.sfs.battle.factories;
+
+import com.gerantech.mmory.core.battle.units.Unit;
+import com.gerantech.mmory.sfs.battle.BattleRoom;
+
+public class ZoneEndCalculator extends EndCalculator
+{
+    public int round = 1;
+    public ZoneEndCalculator(BattleRoom room)
+    {
+        super(room);
+    }
+
+    @Override
+    public boolean check()
+    {
+        if( super.check() )
+            return true;
+        Unit flag = room.battleField.units.__get(0);
+        if( flag.health < flag.card.health )
+            return false;
+        
+        int side = flag.side + 3;
+        this.room.trace("zone captured by " + side);
+        this.round ++;
+        this.scores[side] ++;
+
+        this.room.sendNewRoundResponse(side, -1);
+        if( this.scores[side] > 2 )
+            return true;
+
+        this.room.battleField.requestKillPioneers(-1);
+        return false;
+    }
+}
